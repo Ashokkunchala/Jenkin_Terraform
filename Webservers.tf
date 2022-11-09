919ad8ae31aa65f666581ae17d4da83868b97ffc
Kunchala Ashok <93498307+Ashokkunchala@users.noreply.github.com>
2022-11-09 10:16:41 +0530

    Update Webservers.tf

--- Webservers.tf
+++ Webservers.tf
@@ -8,3 +8,9 @@ resource "aws_instance" "ec2-example" {
            Name = "Jenkins_Terra_Ec2"
   }
 }
+
+variable "instance_count" {
+  description = "Ec2 instance count"
+  type = number
+  default = 1
+}
